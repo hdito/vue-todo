46e6166198dfc293079d7328e7f7c736567930d9
hdito <hditow@gmail.com>
2022-12-29 22:08:32 +0300

    Organize classes with Prettier

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,30 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="max-w-lg m-auto p-4 space-y-4">
+  <div class="m-auto max-w-lg space-y-4 p-4">
     <h1 class="text-center text-2xl font-bold">Vue ToDo</h1>
     <form
-      class="shadow-md shadow-slate-500 rounded-md max-w-xs m-auto flex flex-col p-2 gap-2"
+      class="m-auto flex max-w-xs flex-col gap-2 rounded-md p-2 shadow-md shadow-slate-500"
     >
       <label class="flex flex-col">
         <span>New task</span>
-        <input class="border rounded p-1 border-slate-300 px-2" type="text" />
+        <input class="rounded border border-slate-300 p-1 px-2" type="text" />
       </label>
       <button
-        class="w-full bg-blue-500 text-white p-1 rounded-md hover:shadow-md"
+        class="w-full rounded-md bg-blue-500 p-1 text-white hover:shadow-md"
       >
         Add
       </button>
     </form>
     <div
-      class="shadow-md shadow-slate-500 rounded-md pl-4 pr-2 py-2 flex gap-4 items-center"
+      class="flex items-center gap-4 rounded-md py-2 pl-4 pr-2 shadow-md shadow-slate-500"
     >
-      <label class="flex gap-2 flex-1">
+      <label class="flex flex-1 gap-2">
         <input class="peer scale-150" type="checkbox" id="toggle" />
         <span class="peer-checked:line-through">ToDo</span>
       </label>
       <button
-        class="flex flex-col py-0.5 px-2 items-center hover:shadow-md rounded-md transition-shadow"
+        class="flex flex-col items-center rounded-md py-0.5 px-2 transition-shadow hover:shadow-md"
       >
         <v-icon scale="1.75" name="bi-trash" />
         <span class="text-sm">Delete</span>
